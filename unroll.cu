0606d8f420ccdfa96974e57a4238f378057d9432
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-02-28 14:47:18 -0800

    add2

--- unroll.cu
+++ unroll.cu
@@ -82,9 +82,12 @@ kernel4(dtype *g_idata, dtype *g_odata, unsigned int n)
     __syncthreads ();
     }
     if (threadIdx.x < 32) {
-
-        scratch[threadIdx.x] += scratch[threadIdx.x + 32];
-        scratch[threadIdx.x] += scratch[threadIdx.x + 16];
+        if (n > 64) {
+            scratch[threadIdx.x] += scratch[threadIdx.x + 32];
+        }
+        if (n > 32) {
+            scratch[threadIdx.x] += scratch[threadIdx.x + 16];
+        }
         scratch[threadIdx.x] += scratch[threadIdx.x+8];
         scratch[threadIdx.x] += scratch[threadIdx.x+4];
         scratch[threadIdx.x] += scratch[threadIdx.x+2];
